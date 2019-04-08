89ef010aba8966db2bfa9ffe2ba23187bb2ecc62
Akshal Aniche <aniche.akshal@gmail.com>
2019-04-08 15:58:19 -0400

    Opening moves

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,35 @@
 package student_player;
 
+import java.util.Random;
+
+import pentago_swap.PentagoMove;
+import pentago_swap.PentagoBoardState.Quadrant;
+
 public class MyTools {
     public static double getSomething() {
         return Math.random();
+    }
+    
+    /**
+     * Generates 4 opening moves, with random quadrant swap.
+     * The moves returned place a piece in the center of one of the four quadrants.
+     * @param rand
+     * @param playerID
+     * @return
+     */
+    public static PentagoMove[] getOpening(Random rand, int playerID){
+    	int[] centers = {1, 4};
+    	Quadrant[] quads = Quadrant.values();
+    	PentagoMove[] openingMoves = new PentagoMove[4];
+    	for (int i = 0; i < 4; i++){
+    		int quadA = rand.nextInt(4);
+    		int quadB = rand.nextInt(3);
+    		if (quadB >= quadA) quadB++;
+    		
+    		openingMoves[i] = new PentagoMove(centers[i%2], centers[i/2], quads[quadA], quads[quadB], playerID);
+    		//System.out.println(openingMoves[i].toPrettyString());
+    	}
+    	
+    	return openingMoves;
     }
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -1,9 +1,12 @@
 package student_player;
 
+import java.util.Random;
+
 import boardgame.Move;
 
 import pentago_swap.PentagoPlayer;
 import pentago_swap.PentagoBoardState;
+import pentago_swap.PentagoMove;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends PentagoPlayer {
@@ -14,9 +17,11 @@ public class StudentPlayer extends PentagoPlayer {
      * associate you with your agent. The constructor should do nothing else.
      */
     public StudentPlayer() {
-        super("xxxxxxxxx");
+        super("260726335");
     }
 
+    private static Random rand;
+    private PentagoMove[] openingMoves;
     /**
      * This is the primary method that you need to implement. The ``boardState``
      * object contains the current state of the game, which your agent must use to
@@ -28,7 +33,29 @@ public class StudentPlayer extends PentagoPlayer {
         // strategies...
         MyTools.getSomething();
 
+        int turn = boardState.getTurnNumber();
+        int player = boardState.getTurnPlayer();
+        
+        if (turn == 0){
+        	rand = new Random(2019);
+        	this.openingMoves = MyTools.getOpening(rand, player);
+        }
+        if (turn < 4) {
+        	int i = 0;
+        	while (i < 4) {
+        		PentagoMove move = this.openingMoves[i];
+        		if (boardState.isLegal(move)){
+        			return move;
+        		}
+        		i++;
+        	}
+        }else 
+        {
+        	
+        }
+        
         Move myMove = boardState.getRandomMove();
+        
 
         // Return your move to be processed by the server.
         return myMove;
